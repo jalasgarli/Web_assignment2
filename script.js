08008c28362332701b5540931b6380679921ad90
javidalasgarli <alasgarlijavid@gmail.com>
2023-11-30 23:00:53 +0400

    Search Bar Added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const productsContainer = document.getElementById('products');
-    
+    const searchInput = document.getElementById('search_keyword');
+    console.log(search);
     fetch('https://dummyjson.com/products')
         .then(response => {
             if (!response.ok) {
@@ -9,26 +10,54 @@ document.addEventListener('DOMContentLoaded', function () {
             return response.json();
         })
         .then(data => {
-            fetchProducts(data, productsContainer);
+            const product_array = data.products;
+            fetchProducts(product_array, productsContainer);
+
+
+            searchInput.addEventListener('input', function () {
+                const searchKeword = searchInput.value.toLowerCase();
+                const filteredProducts =  filterProducts(product_array, searchKeword);
+                displayProducts(filteredProducts, productsContainer);
+                console.log(searchKeword);
+            });
         })
         .catch(error => {
             console.error('Error fetching data:', error.message);
-        });
+    });
+
+    
 });
 
-function fetchProducts(productsData, container) {
-    const productsArray = productsData.products;
-
+function fetchProducts(productsArray, container) {
+    console.log(productsArray);
+    container.innerHTML = ``;
     productsArray.forEach(product => {
         container.innerHTML += `
-            <div class="product">
+            <div class="product" id="${product.id}">
                 <img class="product_img" src="${product.thumbnail}" alt="${product.title}">
-                <h3 class="product_title">${product.title}</h3>
-                <p class="product-price">${product.price}</p>
-                <p class="product_discount">${product.discount}</p>
-                <p class="product_category">${product.category}</p>
-                <p class="product_stock">${product.stock}</p>
+                <h4 class="product_title">${product.title}</h4>
+                <div class="product_data"> 
+                    <p class="product_price">Price: ${product.price}</p>
+                    <p class="product_discount">Discount: ${product.discountPercentage}</p>
+                    <p class="product_category">Category: ${product.category}</p>
+                    <p class="product_stock">Stock: ${product.stock}</p>
+                </div>
             </div>
+  
         `;
     });
+}
+
+function filterProducts(productArray, keyword) {
+    return productArray.filter(product => {
+        const title = product.title.toLowerCase().includes(keyword);
+        const category = product.category.toLowerCase().includes(keyword);
+        /// about desc;
+
+        return title || category;
+    })
+}
+
+function displayProducts(productArray, container) {
+    fetchProducts(productArray, container)
 }
